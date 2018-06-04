0f58219e867c07e7d3c98a0950bc1a592ecb45dc
anjan <anjan@w3engineers.com>
2018-06-04 11:50:56 +0600

    methods added

--- app/src/main/java/com/aj/uss/collectionwidget/CollectionWidget.java
+++ app/src/main/java/com/aj/uss/collectionwidget/CollectionWidget.java
@@ -7,6 +7,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -48,23 +50,51 @@ public class CollectionWidget extends AppWidgetProvider {
     }
 
 
-        public static void sendRefreshBroadcast(Context context) {
-            Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.setComponent(new ComponentName(context, CollectionWidget.class));
-            context.sendBroadcast(intent);
-        }
+    public static void sendRefreshBroadcast(Context context) {
+        Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.setComponent(new ComponentName(context, CollectionWidget.class));
+        context.sendBroadcast(intent);
+    }
 
 
-        @Override
-        public void onReceive(final Context context, Intent intent) {
-            final String action = intent.getAction();
-            if (action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-                // refresh all your widgets
-                AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-                ComponentName cn = new ComponentName(context, CollectionWidget.class);
-                mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn), R.id.widgetListView);
-            }
-            super.onReceive(context, intent);
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        final String action = intent.getAction();
+        if (action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+            // refresh all your widgets
+            AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+            ComponentName cn = new ComponentName(context, CollectionWidget.class);
+            mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn), R.id.widgetListView);
         }
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        Log.d("Widget: ","onAppWidgetOptionsChanged");
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d("Widget: ","onEnabled");
+
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d("Widget: ","onDisabled");
+
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+        Log.d("Widget: ","onRestored");
+
+    }
 }
 
