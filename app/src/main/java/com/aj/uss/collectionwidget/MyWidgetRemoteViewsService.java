df97fce872a6d23cecae91ab4cc509bbcbfec988
anjan <anjan@w3engineers.com>
2018-06-04 11:31:53 +0600

    comment added

--- app/src/main/java/com/aj/uss/collectionwidget/MyWidgetRemoteViewsService.java
+++ app/src/main/java/com/aj/uss/collectionwidget/MyWidgetRemoteViewsService.java
@@ -8,6 +8,10 @@ import android.widget.RemoteViewsService;
  * Created by Anjan on 6/3/2018.
  */
 
+/**
+ * The main purpose of RemoteViewsService is to return a RemoteViewsFactory object
+ * which further handles the task of filling the widget with appropriate data.
+ */
 public class MyWidgetRemoteViewsService extends RemoteViewsService {
 
     private static final String TAG = "WidgetService";
